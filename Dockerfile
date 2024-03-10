03531094af800c8d2a615c199cd22612ce5f31fc
Deepak <rohit45deepak@gmail.com>
2024-03-11 03:02:52 +0530

    api done

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install -r requirements.txt
+
+# Create directories for SSL certificates and key
+RUN mkdir -p /app/certs /app/key
+
+# Copy SSL certificates to the container
+COPY xdba_certificate.pem /app/certs/
+COPY test.pem /app/key/
+
+
+
+# Make port 8000 available to the world outside this container
+EXPOSE 8000
+
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches with SSL support
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload", "--ssl-keyfile", "/app/key/test.pem", "--ssl-certfile", "/app/certs/xdba_certificate.pem"]
--- linkedin.py
+++ linkedin.py
@@ -8,15 +8,15 @@ import time
 from bs4 import BeautifulSoup
 async def openBrowser():
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-software-rasterizer")
     driver = webdriver.Chrome(options=options)
-    # driver.set_window_size(1920, 1080)
-    driver.maximize_window()
+    driver.set_window_size(1920, 1080)
+    # driver.maximize_window()
     return driver
     
 def openBrowserUserCookies(cookies):
@@ -30,6 +30,7 @@ def openBrowserUserCookies(cookies):
     driver = webdriver.Chrome(options=options)
     for cookie in cookies:
         driver.add_cookie(cookie)
+    driver.set_window_size(1920, 1080)
     driver.get("https://www.linkedin.com")
     return driver
     

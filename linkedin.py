f6657a8a5985985fee74de657f53d1289c5e3e8f
Deepak <rohit45deepak@gmail.com>
2024-03-15 04:41:52 +0530

    api

--- linkedin.py
+++ linkedin.py
@@ -42,7 +42,7 @@ async def openBrowser():
     options = Options()
     
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument(f'user-agent={user_agent}')
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")

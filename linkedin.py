49a61920ff8989aa755c6744bebbba98d37eef9c
Deepak <rohit45deepak@gmail.com>
2024-03-23 23:03:04 +0530

    time message

--- linkedin.py
+++ linkedin.py
@@ -18,7 +18,7 @@ async def openBrowser(proxy_address, proxy_port, proxy_username, proxy_password 
         }
     }
     # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     # options.add_argument(f'user-agent={user_agent}')
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
@@ -52,7 +52,7 @@ async def openBrowserUserCookies(cookies, proxy_address, proxy_port, proxy_usern
         }
     }
     # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     # options.add_argument(f'user-agent={user_agent}')
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
--- main.py
+++ main.py
@@ -24,8 +24,8 @@ app = FastAPI()
 origins = [
     "https://leads.sddoc.in",
     "https://leads.sddoc.in/",
-    "http://v3.newson.io",
-    "http://v3.newson.io/",
+    "https://v3.newson.io",
+    "https://v3.newson.io/",
     "http://localhost:3000",
     "http://localhost:3000/",
 ]
